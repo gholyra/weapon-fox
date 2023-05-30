b64f0aeecc346bdb88b1afa216ae6b8cb9173c9a
Tiago Marchetti Lyra <tiago.mlyra@gmail.com>
2023-05-29 21:35:58 -0300

    3º Commit do Projeto
    Mais desenvolvimento da arma e do projétil

--- WeaponFox/Assets/Scripts/Bullet.cs
+++ WeaponFox/Assets/Scripts/Bullet.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] private float bulletVelocity = 100f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(LifeSpan());
+    }
+
+    private IEnumerator LifeSpan()
+    {
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.right * bulletVelocity * Time.deltaTime);
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("colliding");
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
 }
